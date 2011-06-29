3816cb4749d24308da6fbdf0ad52759a60ca5e11
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-06-29 11:20:02 -0400

    implement opencore auth; add plugin config var

--- zine/plugins/opencore_auth/__init__.py
+++ zine/plugins/opencore_auth/__init__.py
@@ -6,14 +6,30 @@
     Use OpenCore authentication instead of Zine's built-in authentication.
 """
 
+from Cookie import BaseCookie
 from libopencore import auth
 from zine.application import Request as RequestBase
+from zine.database import db
+from zine.utils import forms
+from zine.models import User
 
-class Request(BaseRequest):
+class Request(RequestBase):
     def get_user(self):
         app = self.app
-        secret = app.cfg['opencore_shared_secret_filename']
-        # ...
+        secret = app.cfg['opencore_auth/shared_secret_filename']
+        secret = auth.get_secret(secret)
+        try:
+            morsel = BaseCookie(self.environ['HTTP_COOKIE'])['__ac']
+            username, hash = auth.authenticate_from_cookie(
+                morsel.value, secret)
+        except (KeyError, auth.BadCookie, auth.NotAuthenticated):
+            return None
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            user = User(username, None, "test@example.com")
+            db.session.add(user)
+            db.commit()
+        return user
 
     def __init__(self, environ, app=None):
         RequestBase.__init__(self, environ)
@@ -21,3 +37,5 @@ class Request(BaseRequest):
 
 def setup(app, plugin):
     app._request_class = Request
+    app.add_config_var('opencore_auth/shared_secret_filename',
+                       forms.TextField(default=''))
