806cd9d3b6f8ab57b67bf70b4b8f40f0221cd8cd
Ethan Jucovy <ejucovy@gmail.com>
2011-06-29 08:50:15 -0400

    start to write an opencore auth plugin using the User.request_groups, Request.get_user and Application._request_class hooks

---
+++ zine/plugins/opencore_auth/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+    zine.plugins.opencore_auth
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Use OpenCore authentication instead of Zine's built-in authentication.
+"""
+
+from libopencore import auth
+from zine.application import Request as RequestBase
+
+class Request(BaseRequest):
+    def get_user(self):
+        app = self.app
+        secret = app.cfg['opencore_shared_secret_filename']
+        # ...
+
+    def __init__(self, environ, app=None):
+        RequestBase.__init__(self, environ)
+        #self.user.request_groups.append(...)
+
+def setup(app, plugin):
+    app.add_parser('creole', CreoleParser)
